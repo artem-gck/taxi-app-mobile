91447009f020cb8fcabb3dd0d8fc80297f53a6f5
Artem Hatsko <72604580+artem-gck@users.noreply.github.com>
2023-02-20 12:33:53 +0100

    Refactor for mvvm pattern

---
+++ Taxi Mobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+using Taxi_mobile.Infrastructure;
+
+namespace Taxi_mobile.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private int _count = 0;
+        private string _text = "Not pressed";
+
+        public string Text { get => _text; set => SetProperty(ref _text, value); }
+
+        public ICommand Command => new Command(() =>
+        {
+            _count++;
+
+            if (_count == 1)
+                Text = $"Clicked {_count} time";
+            else
+                Text = $"Clicked {_count} times";
+        });
+    }
+}
---
+++ Taxi Mobile/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Taxi_mobile.ViewModels;
+
+namespace Taxi_mobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = new MainPageViewModel();
+    }
+}
+
